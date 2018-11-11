9bff30ee9f839ccc4f197c2ba7976624ad7fcc7b
Katrina Steinman <ksteinma@mines.edu>
2018-11-11 16:04:02 -0700

    Functioning rectangle without orthophoto, with open and close

--- identify_rectangle.m
+++ identify_rectangle.m
@@ -1,29 +1,15 @@
-I = imread('im3.JPG');
-%I = imresize(I, 0.3);
+I = imread('images/im3.JPG');
 
 [height width colors] = size(I);
 
 level = graythresh(I);
 G = imbinarize(I, level);
-% E = edge(G, 'canny');
-% imshow(E)
-
-
-% [mserRegions, mserConnComp] = detectMSERFeatures(G, 'RegionAreaRange',[height width],'ThresholdDelta',0.001);
-% 
-% figure
-% imshow(I)
-% hold on
-% plot(mserRegions, 'showPixelList', true,'showEllipses',false)
-% title('MSER regions')
-% hold off   
+se = strel('square', 2);
+G = imclose(G, se);
+G = imopen(G, se);
 
 O = ocr(G);
-%word = O.Words{2};
-%wordBBox = O.WordBoundingBoxes(2,:);
-%figure;
-%Iname = insertObjectAnnotation(I, 'rectangle', wordBBox, word);
-%imshow(Iname);
+best = O;
 best_R = G;
 % find best confidence
 max_conf = 0;
@@ -34,19 +20,23 @@ for angle = -20:1:20
     if(size(O.CharacterConfidences, 1) > max_conf)
        max_conf = size(O.CharacterConfidences, 1);
        best_R = R;
+       best = O;
        best_angle = angle;
     end
     close all
 end
 
-b = ocr(best_R);
-b = b.WordBoundingBoxes;
+best = ocr(best_R);
+b = best.WordBoundingBoxes;
 
 min_height = inf;
 max_height = 0;
 min_width = inf;
 max_width = 0;
 for i=1:size(b,1)
+    if best.Words{i} == ' '
+       continue 
+    end
     if(b(i,1) < min_height)
        min_height = b(i,1); 
     end
@@ -64,38 +54,51 @@ for i=1:size(b,1)
     end
 end
 
-% pts = insertMarker(I, [min_height min_width], 'color', 'red', 'size', 10);
-% pts = insertMarker(pts, [min_height max_width], 'color', 'green', 'size', 10);
-% pts = insertMarker(pts, [max_height min_width], 'color', 'white', 'size', 10);
-% pts = insertMarker(pts, [max_height max_width], 'color', 'blue', 'size', 10);
-% figure
-% imshow(pts)
 result = imrotate(I, best_angle);
-result = result(:,:,1);
 imshow(result)
 line([min_height min_height], [min_width max_width], 'LineWidth', 2);
 line([min_height max_height], [max_width max_width], 'LineWidth', 2);
 line([max_height max_height], [max_width min_width], 'LineWidth', 2);
 line([max_height min_height], [min_width min_width], 'LineWidth', 2);
 
-% wordBBox = b(:,:);
-% figure;
-% Iname = insertObjectAnnotation(result, 'rectangle', wordBBox, word);
-% imshow(Iname);
-
-
+% result
+ %show word boxes
+%  Iname = insertObjectAnnotation(result, 'rectangle', best.WordBoundingBoxes, best.WordConfidences);
+% figure
+% imshow(Iname)
 
 %% 
 % orthophoto
+% Ipts = [min_height min_width;
+%         min_height max_width;
+%         max_height min_width;
+%         max_height max_width;];
+%     
+% Wpts = [0 0;
+%         0 max_width-min_width;
+%         max_height-min_height 0;
+%         max_height-min_height max_width-min_width];
+%     
+% transform = fitgeotrans(Ipts, Wpts, 'projective');
+% [ortho, ref]= imwarp(result, transform);
+% figure
+% imshow(ortho)
+% title('Orthophoto')
+% 
+% [X,Y] = transformPointsForward(transform, Ipts(:,1), Ipts(:,2));
+% 
+% X = X - ref.XWorldLimits(1);
+% Y = Y - ref.YWorldLimits(1);
+%  line([X(1) X(2)], [Y(1) Y(2)], 'LineWidth', 2);
+%  line([X(2) X(4)], [Y(2) Y(4)], 'LineWidth', 2);
+%  line([X(3) X(4)], [Y(3) Y(4)],'LineWidth', 2);
+%  line([X(3) X(1)], [Y(3) Y(1)],'LineWidth', 2);
 
-%  [min_height min_width]
-%  [min_height max_width]
-%  [max_height min_width]
-%  [max_height max_width]
-file = fopen('rectangle.txt', 'w');
+%% write out
+file = fopen('temp/rectangle.txt', 'w');
 fprintf(file, '%d %d ', min_width, min_height);
 fprintf(file, '%d %d', (max_width - min_width), (max_height-min_height));
 fclose(file);
-
+saveas(gcf, 'temp/post_processed.png');
 
 
