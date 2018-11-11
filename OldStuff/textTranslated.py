d5b5b6aa0097ef14cfbf9879f9c5b8baea1f694a
krampcg23 <krampcg23@gmail.com>
2018-11-11 12:58:20 -0700

    Added to main

--- OldStuff/textTranslated.py
+++
@@ -1,7 +0,0 @@
-from googletrans import Translator
-
-if __name__ == "__main__":
-    translator = Translator()
-    translation = translator.translate('apple pie is my favorite', dest='es')
-    print(translation.origin, ' -> ', translation.text)
-
--- main.cpp
+++ main.cpp
@@ -2,14 +2,45 @@
 #include <tesseract/baseapi.h>
 #include <leptonica/allheaders.h>
 #include <opencv2/opencv.hpp>
- 
+#include <map>
+#include <fstream>
+#include <algorithm> 
 using namespace std;
 using namespace cv;
+
+void filterAndAdd(string word, map<string, int> dictionary, string &filteredText) {
+    transform(word.begin(), word.end(), word.begin(), ::tolower);
+    if (dictionary[word] > 0) {
+        filteredText += word;
+    }
+}
  
+void addCharAndReset(char s, string &filteredText, string &word) {
+    filteredText += s;  word = "";
+    if (s != '\n') {
+        filteredText += ' ';
+    }
+}
+
 int main(int argc, char* argv[])
 {
     string outText;
     string imPath = argv[1];
+
+    ifstream fin("dictionary.txt");
+    if (!fin) {
+	fin.clear();
+	cerr << "Error opening file \"" << "dictionary.txt" << "\"!" << endl;
+        return 1;
+    }
+    string word;
+    map<string, int> dictionary;
+    while(!fin.eof()) {
+        fin >> word;
+        dictionary[word] = 1;
+    }
+
+    fin.close();
  
     // Create Tesseract object
     tesseract::TessBaseAPI *ocr = new tesseract::TessBaseAPI();
@@ -28,9 +59,47 @@ int main(int argc, char* argv[])
  
     // Run Tesseract OCR on image
     outText = string(ocr->GetUTF8Text());
- 
-    // print recognized text
-    cout << outText << endl; // Destroy used object and release memory ocr->End();
-   
-    return EXIT_SUCCESS;
+    
+    /*cout << "====== ORIGINAL TESSERACT OUTPUT =====" << endl;
+    cout << outText << endl;
+    cout << "++++++++++++++++++++++++++++++++++++++" << endl << endl;*/
+
+    string filteredText;
+    word = "";
+    for (char s : outText) {
+        switch (s) {
+            case ' ':
+                filterAndAdd(word, dictionary, filteredText);
+                filteredText += ' ';
+                word = ""; break;
+            case '\n':
+                filterAndAdd(word, dictionary, filteredText);
+                addCharAndReset(s, filteredText, word); break;
+            case ',':
+                filterAndAdd(word, dictionary, filteredText);
+                addCharAndReset(s, filteredText, word); break;
+            case '.':
+                filterAndAdd(word, dictionary, filteredText);
+                addCharAndReset(s, filteredText, word); break;
+            case '!':
+                filterAndAdd(word, dictionary, filteredText);
+                addCharAndReset(s, filteredText, word); break;
+            case '?':
+                filterAndAdd(word, dictionary, filteredText);
+                addCharAndReset(s, filteredText, word); break;
+            default:
+                word += s;
+        }
+    }
+
+   /* cout << "====== FILTERED DICTIONARY OUTPUT =====" << endl;
+    cout << filteredText << endl;
+    cout << "+++++++++++++++++++++++++++++++++++++++" << endl;*/
+
+    ofstream textFile;
+    textFile.open ("temp/filteredText.txt");
+    textFile << filteredText;
+    textFile.close();
+
+    return 0;
 }
